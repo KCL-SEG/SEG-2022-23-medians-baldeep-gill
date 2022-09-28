5abe4babc8f5e74f8e99d6514e52f52a9d6cbf8c
Baldeep Gill <baldeep.gill@kcl.ac.uk>
2022-09-28 21:26:55 +0100

    Complete first attempt at medians exercise

--- median.py
+++ median.py
@@ -9,4 +9,13 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+length = len(numbers)
+numbers.sort()
+if length == 1:
+    print(numbers[0])
+elif length % 2 == 0:
+    average = (numbers[length//2] + numbers[(length//2) - 1]) / 2
+    print(average)
+else:
+    print(numbers[length // 2])
